08fc267be7788e14e82de527c3ed86db37cf6997
Cesaryuan <Cesaryuan@qq.com>
2023-05-14 20:58:09 +0800

    docs: add install script

---
+++ install.bat
@@ -0,0 +1,6 @@
+@echo off
+
+@REM Run install.ps1 with PowerShell
+Powershell.exe -executionpolicy remotesigned -File .\install.ps1
+
+pause
---
+++ install.ps1
@@ -0,0 +1,21 @@
+﻿# get running script's directory name (not path)
+$ScriptDir = Split-Path $PSScriptRoot -Leaf
+write-host "ScriptDir: $ScriptDir"
+# make link
+cmd /c mklink /D "..\resources\app\app_launcher\qqnt-tools" "..\..\..\$ScriptDir\dist"
+
+Get-ChildItem "..\resources\app\app_launcher\index.js" -Include "*.js" -Recurse | ForEach-Object {
+    # read with LF
+    $content = Get-Content $_.FullName -Raw
+    $snippet = "require('./qqnt-tools/qq-main.js')"
+    if($content -match "(?s)// Cesar.*?// Cesar"){
+        $content = $content -replace "(?s)// Cesar.*?// Cesar", "// Cesar`n$snippet`n// Cesar";
+    }
+    else{
+        $content = "// Cesar`n$snippet`n// Cesar`n$content";
+    }
+    Set-Content $_.FullName $content
+}
+
+# Wait for user close the window
+Write-Host "安装完成，请重启QQ！"
